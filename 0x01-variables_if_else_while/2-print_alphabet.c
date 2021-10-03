e3e941b56213cc8f2022b7b13d1082bf64148983
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-03 16:06:27 -0700

    0x01-02

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,22 +3,22 @@
 #include <stdio.h>
 
 /**
- * main - prints the last digit of the random
- * number stored in the variable n
- * Return: Always 0 (Success)
- * the string and is greater than 5
- * the string and is 0
- * the string and is less than 6 and not 0
- */
+* main - prints the last digit of the random
+* number stored in the variable n
+* Return: Always 0 (Success)
+* the string and is greater than 5
+* the string and is 0
+* the string and is less than 6 and not 0
+*/
 int main(void)
 {
-  int i=97;
-  while (i < 123)
-    {
-      putchar(i);
-      i++;
-    }
-  putchar('\n');
-  return (0);
+int i = 97;
+while (i < 123)
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
 }
 
