70399a3188be385dc7b1f6bbeaa907862b5a8c0b
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-03 16:01:00 -0700

    0x01-02

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints the last digit of the random
+ * number stored in the variable n
+ * Return: Always 0 (Success)
+ * the string and is greater than 5
+ * the string and is 0
+ * the string and is less than 6 and not 0
+ */
+int main(void)
+{
+        int i=97;
+	while (i < 123)
+	  {
+	  putchar(i);
+	  i++;
+	  }
+	putchar('\n');
+	return (0);
+}
+
