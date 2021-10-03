733b5eb53cfd8c47639205e878af817ceb87a1af
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-03 15:38:30 -0700

    0x01-1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- *if the last digit of n is greater than 5: the string and is greater than 5
- *if the last digit of n is 0: the string and is 0
- *if the last digit of n is less than 6 and not 0: the string and is less than 6 and not 0
+ *the string and is greater than 5
+ *the string and is 0
+ *the string and is less than 6 and not 0
  */
 int main(void)
 {
