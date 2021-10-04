50da208e04d7f9b8e7520a552fe1d09b5ab1a283
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-04 04:51:57 -0700

    0x01-8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - prints the last digit of the random
+* number stored in the variable n
+* Return: Always 0 (Success)
+* the string and is greater than 5
+* the string and is 0
+* the string and is less than 6 and not 0
+*/
+int main(void)
+{
+int i = 48;
+while (i < 58)
+{
+putchar(i);
+i++;
+}
+i = 97;
+while (i < 103)
+{
+putchar(i);
+i++;
+} 
+putchar('\n');
+return(0);
+} 
