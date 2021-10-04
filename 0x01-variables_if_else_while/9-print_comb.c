cfc3a983811295cb1f786a8e08d119bc8900088a
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-04 05:17:05 -0700

    0x01-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - prints the last digit of the random
+* number stored in the variable n
+* Return: Always 0 (Success)
+* the string and is greater than 5
+* the string and is 0
+* the string and is less than 6 and not 0
+*/
+int main(void)
+{
+int i = 48;
+while (i <= 57)
+{
+putchar(i);
+if ( i != 57 )
+{
+putchar(44);
+putchar(32);
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
