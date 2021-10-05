6c61c192b7961d21c49c2ab06f0a584b04f6d130
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-05 12:57:03 -0700

    0x02-00

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - prints the last digit of the random
+* number stored in the variable n
+* Return: Always 0 (Success)
+* the string and is greater than 5
+* the string and is 0
+* the string and is less than 6 and not 0
+*/
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
