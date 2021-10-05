51241c90336de9f30c623f1fb140619f54a42109
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-05 14:58:53 -0700

    0x02-06

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* _abs - prints the last digit of the random
+* @n : integer
+* Return: Always 0 (Success)
+* the string and is greater than 5
+* the string and is 0
+* the string and is less than 6 and not 0
+*/
+int _abs(int n)
+{
+if (n < 0)
+{
+n = -n;
+return (n);
+}
+else
+return (n);
+}
