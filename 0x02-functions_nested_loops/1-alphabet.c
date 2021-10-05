86389714f7d10ed4cd0e50435c787a6520db3962
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-05 13:30:56 -0700

    0x02-01

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,9 +2,8 @@
 #include "main.h"
 
 /**
-* main - prints the last digit of the random
+*print_alphabet - prints the alphabet
 * number stored in the variable n
-* Return: Always 0 (Success)
 * the string and is greater than 5
 * the string and is 0
 * the string and is less than 6 and not 0
