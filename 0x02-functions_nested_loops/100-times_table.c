e24f00ece43c7d1fdcaba6a94e752affba9ce9e4
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-06 06:43:46 -0700

    0x02-100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,23 +8,26 @@
 void print_times_table(int n)
 {
 int x, y;
+if ((n >= 0) && (n <= 15))
+{
 for (x = 0; x <= n; x++)
 {
 for (y = 0; y <= n; y++)
 {
 if (y == 0)
 {
-  printf("%d", y);
+printf("0");
+continue ;
 }
-else if (y * x <= n)
-{
-printf(",  %d", y * x);
-}
+if ((y * x) < 10)
+printf(",   ");
+else if ((y * x) < 100)
+printf(",  ");
 else
-{
-printf(", %d%d", (y * x) % 10, (y * x) / 10);
-}
+printf(", ");
+printf("%d", y * x);
 }
 printf("\n");
+}
 }
 }
