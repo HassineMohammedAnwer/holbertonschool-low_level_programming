24a8f0dbb2d47c593c0add8be007dd8ef64b7929
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-06 15:07:16 -0700

    0x02-101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
+
 /**
-* main : ojoj
+* main - computes and prints the sum of all the multiples
+* of 3 or 5 below 1024
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-/**
-* sum : ojoj
-* i: Always 0 (Success)
-*/
 int sum = 0;
 int i;
 for (i = 6; i < 1024 ; i++)
