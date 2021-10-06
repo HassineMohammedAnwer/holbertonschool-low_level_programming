9617292abf820e22a4722a6c16f11f25f7775abd
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-06 15:03:45 -0700

    0x02-101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,12 +2,13 @@
 /**
 * main : ojoj
 * Return: Always 0 (Success)
-* the string and is greater than 5
-* the string and is 0
-* the string and is less than 6 and not 0
 */
 int main(void)
 {
+/**
+* sum : ojoj
+* i: Always 0 (Success)
+*/
 int sum = 0;
 int i;
 for (i = 6; i < 1024 ; i++)
@@ -15,6 +16,6 @@ for (i = 6; i < 1024 ; i++)
 if ((i % 5 == 0) || (i % 3 == 0))
 sum = sum + i;
 }
-printf("%d \n", 8+sum);
+printf("%d\n", 8 + sum);
 return (0);
 }
