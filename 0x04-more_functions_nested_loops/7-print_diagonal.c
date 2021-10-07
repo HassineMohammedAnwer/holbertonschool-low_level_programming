38cf5046bb6fd0ca789489c75d52106261c02e98
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-07 13:21:39 -0700

    0x04-07

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,17 +7,21 @@
 void print_diagonal(int n)
 {
 	int i, j;
+
 	if (n > 0)
 	{
-		for (i = 0; i < n; i++)
+		for (i= 0; i < n; i++)
 		{
 			for (j = 0; j < i; j++)
-			_putchar(' ');
-		_putchar('\\');
-		if (i == n - 1)
-			continue;
-		_putchar('\n');
+				_putchar(' ');
+			_putchar('\\');
+
+			if (i == n - 1)
+				continue;
+
+			_putchar('\n');
 		}
 	}
+
 	_putchar('\n');
 }
