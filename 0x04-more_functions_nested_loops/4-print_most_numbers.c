cfda126cb5b903e95307c7c3986a74422a881e4b
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-07 12:11:51 -0700

    0x04-04

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,14 +3,14 @@
 
 /**
  * print_most_numbers - function function that prints the numbers, from 0 to 9 except 2 and 4
- *
+ * Return: nothing
  */
 void print_most_numbers(void)
 {
 int i;
 for (i = 0; i <= 9; i++)
 {
-if ((i != 2) && (i != 4))
+if (i != 2 && i != 4)
 {
 _putchar((i % 10) + '0');
 }
