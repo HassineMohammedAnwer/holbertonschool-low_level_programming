093ce3ee327334a24ccc102475586fb7d8a2350f
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-07 13:23:44 -0700

    0x04-07

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_diagonal - a function that draws a diagonal line on the terminal.
- * @n: he number of times the character \ should be printed
+ * print_diagonal - Draws a diagonal line using the \ character.
+ * @n: The number of \ characters to be printed.
  */
 void print_diagonal(int n)
 {
