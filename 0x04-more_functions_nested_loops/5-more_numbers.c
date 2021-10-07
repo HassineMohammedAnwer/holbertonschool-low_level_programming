a0cca07dac5b20ddaaab2a58c96e3c34f9c85e9d
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-07 12:24:23 -0700

    0x04-05

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers -  prints the numbers, from 0 to 9 14 times
+ *
+ * Return: none
+ */
+void more_numbers(void)
+{
+int i,j;
+ for (j = 0; j < 14; j++)
+   {
+	for (i = 0; i <= 9; i++)
+	{
+	if (i > 9)
+	_putchar((i /10) + '0');
+	_putchar((i % 10) + '0');
+	}
+	_putchar ('\n');
+}
+}
