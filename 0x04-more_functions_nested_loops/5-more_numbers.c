e5801db5cf24010fca97dbba1adfb2019276ac33
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-07 12:41:29 -0700

    0x04-05

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,15 +6,15 @@
  */
 void more_numbers(void)
 {
-int i,j;
-for (j = 0; j < 14; j++)
-{
-for (i = 0; i <= 9; i++)
-{
-if (i > 9)
-_putchar((i /10) + '0');
-_putchar((i % 10) + '0');
-}
-_putchar ('\n');
-}
+	int i,j;
+	for (j = 0; j < 14; j++)
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			if (i > 9)
+			_putchar((i /10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
