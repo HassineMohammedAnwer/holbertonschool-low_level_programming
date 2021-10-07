a2209e5af8bfa1edd0169f2015992c7dea2d31b3
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-07 15:01:01 -0700

    0x04-100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 /**
- * main - Finds and prints the largest prime factor of the number 612852475143.
+ * main - Finds and prints the largest prime factor
+ *	  of the number 612852475143.
  *
  * Return: Always 0.
  */
