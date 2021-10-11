5fdef6a9965b6754b3bf59bf53441b0b80bba030
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-11 02:16:08 -0700

    0x05-00

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - function hat takes a pointer to an int as parameter and updates the value it points to to 98.
+ * reset_to_98 - function hat updates the value it points to to 98.
  * @n: The character to test
  *
  * Return: none
