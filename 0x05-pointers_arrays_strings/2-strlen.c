fc3962690f59a7a32686657ae6692cd9e43b4190
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-11 02:43:04 -0700

    0x05-02

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: The string to enter
+ *
+ * Return: strlen success
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return(i);
+}
