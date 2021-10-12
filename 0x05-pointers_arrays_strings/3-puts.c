1a8f2ab17149e6ae4346f84ba58ab26a5210fb0f
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-11 17:27:14 -0700

    0x05-03

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
-/*
- *_puts:	Write a function that prints a string, followed by a new line, to stdout.
- *@str:	entry string declared in main.c
- *Return:	None
+/**
+ * _puts - Write a function that prints a string, followed by a new line, to stdout.
+ * @str: entry string declared in main.c
+ * Return:	None
 **/
 
 void _puts(char *str)
