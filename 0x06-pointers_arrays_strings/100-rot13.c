d17e396f3ba4899fd78df0465103b1684d20a6ca
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-14 15:44:11 -0700

    0x06-100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -16,11 +16,15 @@ char *rot13(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 53; j++)
+		for (j = 0; a[j] != '\0'; j++)
 		{
 			if (s[i] == a[j])
+			{
 				s[i] = b[j];
+				break;
+			}
 		}
 	}
+
 	return (s);
 }
