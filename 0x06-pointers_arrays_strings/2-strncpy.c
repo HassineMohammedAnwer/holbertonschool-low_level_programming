3a95bed6145a12b52f47d2ee7edb43fc1f95579a
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-14 13:42:56 -0700

    0x06-02

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - a function that copies a string.
+ * it will use at most n bytes from src
+ * @dest: first entry string
+ * @src: second entry string
+ * @n: third entry integer
+ * Return: dest string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		i++;
+		dest[i] = src [i];
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
