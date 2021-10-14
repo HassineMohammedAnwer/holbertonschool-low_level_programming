adfcec28bea128e263107a2a335003944e02e910
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-14 14:15:48 -0700

    0x06.03

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - a function that compares two strings.
+ *
+ * @s1: first entry string
+ * @s2: second entry string
+ *
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while(*s1 == *s2)
+	{
+		if (*s1 == '\0')
+			return (0);
+		s1++;
+		s2++;
+	}
+	return (s1 - s2);
+}
