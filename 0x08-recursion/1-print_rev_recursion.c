4fdaa4b881fccaa96c9ad6937f1016c00602286c
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-20 15:21:41 -0700

    0x08-01

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse, followed by a new line.
+ * @s: The string to print in reverse
+ *
+ * Return: none.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
