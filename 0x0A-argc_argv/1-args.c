52409ceefec6055d781e234523b0dd8baeb299d2
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-22 11:23:59 -0700

    0X0A-01

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main- prints the name of the program + new line.
+ * @argc : length of argv
+ * @argv : array of the command line arguments (strings)
+ * Return: always 0
+ */
+int main(int argc,__attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
