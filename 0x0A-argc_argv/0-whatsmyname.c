14d7d9eaeddd975b51cdcb88d75e511dfa977a4d
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-22 09:02:30 -0700

    0x0A-00

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,10 +4,10 @@
  * main - prints the name of the program + new line.
  * @argc: length of argv
  * @argv: array of the command line arguments (strings)
- * Return: argc (cause gcc wont work if argc not used.
+ * Return: 0.
  */
-int main(int argc, char* argv[])
+int main(__attribute__((unused))int argc, char* argv[])
 {
 	printf("%s\n", argv[0]);
-	return (argc);
+	return (0);
 }
