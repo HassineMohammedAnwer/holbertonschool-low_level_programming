9efc9c131ea88cdb4194790010ce31f7b2254fda
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-22 08:49:40 -0700

    0x0A-00

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: The string to print
+ *
+ * Return: none.
+ */
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (argc);
+}
