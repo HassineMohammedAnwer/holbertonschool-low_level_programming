d7672bfa09e932c6ea7dfa366812441286ced673
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-26 11:00:39 -0700

    0x0B-00

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 		return ("null");
 	chars = (char *) malloc(sizeof(char) * size);
 	if (chars == NULL)
-		return (NULL);
+		return (0);
 	for (i = 0; i < size; i++)
 		*(chars + i) = c;
 	return (chars);
