158faf294d830a63668809afccf15668629e7dbe
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-26 03:35:23 -0700

    0x0A-100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,30 +9,30 @@
  */
 int main(int argc, char *argv[])
 {
-	int coins = atoi(argv[1]);
-	int cents = 0;
+	int cents = atoi(argv[1]);
+	int coins = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if (atoi(argv[1]) < 0)
+	if (cents < 0)
 		printf("0\n");
-	while (coins > 0)
+	while (cents > 0)
 	{
-		if (coins >= 25)
-			coins = coins - 25;
-		else if (coins >= 10)
-			coins = coins - 10;
-		else if (coins >= 5)
-			coins = coins - 5;
-		else if (coins >= 2)
-			coins = coins - 2;
+		if (cents >= 25)
+			cents = cents - 25;
+		else if (cents >= 10)
+			cents = cents - 10;
+		else if (cents >= 5)
+			cents = cents - 5;
+		else if (cents >= 2)
+			cents = cents - 2;
 		else
-			coins = coins - 1;
-		cents++;
+			cents = cents - 1;
+		coins++;
 	}
-	printf("%d\n", cents);
+	printf("%d\n", coins);
 	return (0);
 }
