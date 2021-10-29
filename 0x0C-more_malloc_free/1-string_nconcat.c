8f889d208b31e4f57e3ae8953843c8e97159dd8b
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-10-29 15:38:15 -0700

    0x0C-100

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * *string_nconcat -  a function that concatenates two strings.
---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * *_realloc -  reallocates a memory block using malloc and free.
+ * @ptr: old pointer
+ * @old_size: is the size, in bytes, of the allocated space for ptr
+ * @new_size: is the new size, in bytes of the new memory block
+ * Return: the pointer to the newly allocated memory.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p, *p1;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
+	p1 = ptr;
+	if (new_size < old_size)
+	{
+		for (i = 0; i <= new_size; i++)
+			p[i] = p1[i];
+	}
+	else
+	{
+		for (i = 0; i <= old_size; i++)
+                        p[i] = p1[i];
+	}
+	free(p);
+	return (p);
+}
