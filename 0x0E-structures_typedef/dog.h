43777fdb7ab7ae8734119839330b5d497ba99b53
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-11-01 11:40:02 -0700

    0x0E-00

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,20 @@
+#ifndef MY_DOG
+#define MY_DOG
+/**
+ * struct dog - Define a new type struct dog with the following elements
+ * @name: type = char *
+ * @age: type = float
+ * @owner: type = char *
+ *
+ * Description: betty
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+/**
+ * my_dog - typedef for struct dog
+ */
 typedef struct dog my_dog;
