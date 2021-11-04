ac32671a029396447e65b238e25ebfd6fa4cf4a2
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-11-04 07:38:15 -0700

    0x10-00

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,6 +14,8 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int sum = 0;
 
+	if (n == 0)
+                return (0);
 	va_start(x, n);
 	for (i = 0; i < n; i++)
 		sum = sum + va_arg(x, int);
