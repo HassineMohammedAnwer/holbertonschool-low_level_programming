897b2f16093c4c98c15a7a552613d5a79fd1d812
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-11-04 09:56:20 -0700

    0x10-01

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - check the code
- * @seperator: the string to be printed between numbers
+ * @separator: the string to be printed between numbers
  * @n: number of parameters
  * Return: none (void)
  */
@@ -12,7 +12,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(x, n);
-	for (i = 0; i <n; i++)
+	for (i = 0; i < n; i++)
 	{
 		if (i != 0)
 			printf("%s%d", separator, va_arg(x, int));
