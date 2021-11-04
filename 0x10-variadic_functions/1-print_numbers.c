047b3b8c5bdf10ac6b04d9775ac26950124f57d5
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-11-04 09:59:18 -0700

    0x10-01

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,10 +14,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(x, n);
 	for (i = 0; i < n; i++)
 	{
-		if (i != 0)
-			printf("%s%d", separator, va_arg(x, int));
-		else
+		if (!separator)
 			printf("%d", va_arg(x, int));
+		else if (separator && i == 0)
+			printf("%d", va_arg(x, int));
+		else
+			printf("%s%d", separator, va_arg(x, int));
 	}
 	va_end(x);
 	printf("\n");
