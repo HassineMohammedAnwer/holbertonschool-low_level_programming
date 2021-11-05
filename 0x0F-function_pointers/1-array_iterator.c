4476cd74e7e032064fcaafd4e4d053424ab363e2
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-11-05 08:58:18 -0700

    0x0F-01

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,14 +1,16 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name as is
- * @name: name of the person
- * @f: pointer to a function
+ * array_iterator - prints a name as is
+ * @array: parameter
+ * @size: long unsigned int
+ * @action: pointer to a function
  * Return: Nothing.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
+
 	if (array != NULL && size != 0 && action != NULL)
 	{
 		for (i = 0; i < size; i++)
