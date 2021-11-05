89cc4692338f7ad948d755a83153403cee1eb9ef
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-11-05 13:35:23 -0700

    0x0F-100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes of its own main fuction
+ * @argc: size of argv
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	return (0);
+}
