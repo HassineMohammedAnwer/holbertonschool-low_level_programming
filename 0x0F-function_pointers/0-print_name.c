4f184731eacb37f1c3b271d46b117376811f1a1b
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-11-05 08:32:56 -0700

    0x0F-00

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,12 +3,12 @@
 /**
  * print_name - prints a name as is
  * @name: name of the person
- *
+ * @f: pointer to a function
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if ( name != NULL && f != NULL)
+	if (name != NULL && f != NULL)
 		f(name);
 }
 
