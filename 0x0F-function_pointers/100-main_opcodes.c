4a1fc70a0532103b2292d7cc2ad2f9927f4a61ae
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-11-05 15:52:56 -0700

    0x0F-100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,11 +10,31 @@
  */
 int main(int argc, char *argv[])
 {
+	int i, size;
+	char *arr;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-
+	size = atoi(argv[1]);
+	if (size > 0)
+	{
+		arr = (char *)main;
+		for (i = 0; i < size; i++)
+		{
+			if (i == size)
+			{
+				printf("%02hhx\n", arr[i]);
+			}
+			printf("%02hhx", arr[i]);
+		}
+	}
+	else
+	{
+		printf("Error\n");
+		exit(2);
+	}
 	return (0);
 }
