99e63c4de560ea433fd8fe6f1f53aa63eeee4ee8
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-11-17 14:16:42 -0800

    0x13-100

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - check the code
+ * @head: head
+ * Return: pointer to the head og reversed list
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *h, *l = NULL;
+
+	while (h)
+	{
+		h = (*head)->next;
+		(*head)->next = l;
+		l = *head;
+		*head = h;
+	}
+	*head = l;
+	return (*head);
+}
