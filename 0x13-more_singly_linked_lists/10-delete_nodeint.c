8e13aa58570f8f36484f3f3db8442f4560af0c0f
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-11-17 13:58:13 -0800

    0x13-10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,10 +10,9 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
-	listint_t *h;
-	listint_t *l;
+	listint_t *h, *l;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 		return (-1);
 	h = *head;
 	l = *head;
@@ -41,7 +40,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(h);
 		return (1);
 	}
-	free(h);
 	free(l);
 	return (-1);
 }
