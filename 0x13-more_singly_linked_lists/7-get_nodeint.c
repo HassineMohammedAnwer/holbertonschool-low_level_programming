1d1abefeb7165a13d36b75257ae08ac8dd2ac16c
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-11-17 08:10:31 -0800

    0x13-07

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,10 +11,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *h;
 	unsigned int i = 0;
 
-	if (!head)
+	if (head == NULL)
 		return (NULL);
 	h = head;
-	while (i < index || h != NULL)
+	while (i < index && h != NULL)
 	{
 		h = h->next;
 		i++;
