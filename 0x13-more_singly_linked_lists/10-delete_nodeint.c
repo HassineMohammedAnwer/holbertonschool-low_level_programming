1d5fe189526f0e2c49f00bfd4d1137d79357ea48
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-11-17 13:18:56 -0800

    0x13-10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - check the code
+ * @head: head
+ * @index: idx int
+ * Return: 1 on success
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *h;
+	listint_t *l;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+	h = *head;
+	l = *head;
+	if (index == 0)
+	{
+		*head = h->next;
+		return (1);
+	}
+	while (h->next)
+	{
+		if (i == index)
+		{
+			l->next = h->next;
+			return (1);
+		}
+		i++;
+		l = h;
+		l = h->next;
+	}
+	if (i == index)
+	{
+		l->next = NULL;
+		return (1);
+	}
+	return (-1);
+}
