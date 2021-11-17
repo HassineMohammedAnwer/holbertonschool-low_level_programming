d0f5a0fdf2262032bbf7a6329044e77500eeb80e
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-11-17 13:08:03 -0800

    0x13-09

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - check the code
+ * @head: pointer to head of the list
+ * @n: int to add
+ * Return: Adress of the new element.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_h, *l;
+	listint_t *h;
+	unsigned int i = 0;
+
+	new_h = malloc(sizeof(listint_t));
+	if (new_h == NULL)
+		return (NULL);
+	new_h->n = n;
+	new_h->next = NULL;
+	if (!head)
+	{
+		return (NULL);
+	}
+	h = *head;
+	if (idx == 0)
+	{
+		new_h->next = h;
+		*head = new_h;
+		return (new_h);
+	}
+	while (h)
+	{
+		if (i == idx)
+		{
+			new_h->next = h;
+			l->next = new_h;
+			return (new_h);
+		}
+		l = h;
+		h = h->next;
+		i++;
+	}
+	if (i == idx)
+	{
+		l->next = new_h;
+		return (new_h);
+	}
+	return (NULL);
+}
