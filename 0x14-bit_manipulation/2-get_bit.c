23ec01396354f4475cbce77e0f77c1e47bc554c0
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-11-19 13:22:36 -0800

    0x14-02

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * f_num - function return length of int and writes it.
+ * @x: int
+ *
+ * Return: counter
+ *
+ */
+unsigned int f_num(size_t u)
+{
+	unsigned int d = 0;
+
+	if (u == 0)
+		return (1);
+	while (u != 0)
+	{
+		u /= 2;
+		d++;
+	}
+	return (d);
+}
+/**
+ * get_bit - check the code.
+ * @n: int(decimal) to check
+ * @index: index
+ * Return: value of bit at index
+ *
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int b;
+	unsigned int d;
+
+	d = f_num(n);
+	if (index > d)
+		return (-1);
+	b = n >> index;
+	return(b & 1);
+}
