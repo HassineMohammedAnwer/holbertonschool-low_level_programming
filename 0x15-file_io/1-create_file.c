7c004c773c0dbe2faab421835be88acffef7fe9f
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-11-22 14:10:58 -0800

    0x15-01

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,9 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (filename == NULL || fd == -1)
-		return (0);
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
 	sizwr = write(fd, text_content, strlen(text_content));
 	if (sizwr == -1)
 		return (-1);
