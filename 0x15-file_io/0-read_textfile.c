7e655c261b607277755be885e2b7503d063f6d5b
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-11-22 13:40:31 -0800

    0x15-00

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,6 +10,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	char *buffer;
+	ssize_t sizrd, sizwr;
 
 	fd = open(filename, O_RDONLY, 0600);
 	if (filename == NULL || fd == -1)
@@ -17,9 +18,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buffer = malloc(sizeof(char) * (letters + 1));
 	if (buffer == NULL)
 		return (0);
-/**	sizerd = read(fd, buffer, letters);
- *	if (sizrd == -1)
- *		return (0);
- */
-	return (write(1, buffer, read(fd, buffer, letters + '\0')));
+	sizrd = read(fd, buffer, letters + '\0');
+	close(fd);
+	sizwr = write(1, buffer, sizrd);
+	if (sizwr == -1)
+		return (0);
+	free(buffer);
+	return (sizrd);
 }
