df051ce32801d6828187fe0cf823f1efadd2cf5c
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2022-01-06 10:12:44 -0800

    0x17-03

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * add_dnodeint_end - function that adds a new node at the end of a dlistint_t list.
+ * add_dnodeint_end - function adds new node at the end of a dlistint_t list.
  * @head: head of the list
  * @n: int to add
  * Return: the address of the new element, or NULL if it failed
