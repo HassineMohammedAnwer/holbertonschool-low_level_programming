21ce0d442e4d957e1892811403ce550190882ba6
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2022-01-10 08:55:21 -0800

    0x00-00000

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -11,3 +11,19 @@ int main(void)
     printf("%d\n", _strlen("My Dyn Lib"));
     return (0);
 }
+/**
+ * _strlen - check the code
+ * @s: string
+ * Return: len.
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s)
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
