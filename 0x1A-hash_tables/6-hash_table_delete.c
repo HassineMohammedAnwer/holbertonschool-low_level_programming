a7aa06e346dadbe4e20d9b6a6edd030c3eb7f57b
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2022-02-10 08:59:53 -0800

    0x1A-06

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - check the code
+ * @ht: hash table
+ *
+ *
+ * Return: None
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int indx;
+	hash_node_t *node;
+
+	if (!ht && !ht->array && !ht->size)
+		return;
+	for (indx = 0; indx < ht->size; indx++)
+	{
+		while (ht->array[indx])
+		{
+			node = ht->array[indx]->next;
+			free(ht->array[indx]->key);
+			free(ht->array[indx]->value);
+			free(ht->array[indx]);
+			ht->array[indx] = node;
+		}
+	}
+	ht->array = NULL;
+	ht->size = 0;
+	free(ht->array);
+	free(ht);
+}
