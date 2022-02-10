544771fa731f11f2b11fa3e875acdeb49d8ada36
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2022-02-10 06:41:50 -0800

    0x1A-00

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - check the code for
+ *
+ * Return: pointer to the newly created hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *h;
+
+	if (size == 0)
+		return (NULL);
+
+	h = malloc(sizeof(hash_table_t));
+	if (!h)
+		return (NULL);
+	h->size = size;
+	h->array = malloc(sizeof(h->array) * size);
+	if (h->array == NULL)
+		return (NULL);
+
+	return (h);
+}
