2fd19c44e8e72825d1f71a4420fdc1dbbad1a171
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-08-31 20:57:30 +0100

    advanced_b_s

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -32,12 +32,6 @@ int advanced_binary(int *array, size_t size, int value)
 			l = m;
 		else if (array[m] < value)
 			f = m + 1;
-	/*	else
-		{
-			while (array[m - 1] == value)
-				m--;
-			return (m);
-		}*/
 	}
 	if (array[m] == value)
 		return (m);
